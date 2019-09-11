b37c1e9ee15e146ee1cc6e0bfbb4852cc0d9e91a
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-09-12 09:25:44 +1200

    expanded model based on action sketch

--- Model.fs
+++ Model.fs
@@ -6,9 +6,13 @@ type Game = {
     players: Player []
     currentPlayerIndex: int
     dealerIndex: int
+    currentPool: int
 }
 and Player = {
     hand: (int * char) list
-    bet: int
+    state: PlayerState
     cash: int
 }
+and PlayerState = 
+    | Playing of hand: (int * char) list * bet: int
+    | Folded
