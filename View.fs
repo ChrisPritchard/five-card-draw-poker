28fc99ce56993debc80b9bd004a8b50e45ecdd0a
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-09-20 10:42:41 +1200

    built render dealing and discards render functions

--- View.fs
+++ View.fs
@@ -1,9 +1,27 @@
 ﻿module View
 
 open Model
+open Terminal
 
-let renderDealing model dispatch = ()
-let renderDiscards model dispatch = ()
+let renderDealing model dispatch =
+    printfn "Dealing player %i..." model.currentPlayerIndex
+    dispatch Deal
+
+let renderDiscards model dispatch =
+    if model.currentPlayerIndex = 0 then
+        printfn "Your cards:"
+        let hand = model.players.[0].hand |> Seq.map printCard |> String.concat " "
+        printfn "%s" hand
+        printfn ""
+        // TODO select for discard
+        printfn "Press enter to continue"
+        readLine () |> ignore
+        dispatch (Discard [])
+    else
+        // TODO AI
+        printfn "Player %i discards %i cards" model.currentPlayerIndex 0
+        dispatch (Discard [])
+
 let renderBetting model dispatch = ()
 let renderReveal winner model dispatch = ()
 let renderGameOver model dispatch = ()
